09b52b99731a0f5fdda2a9d0b7c0acb035ec46aa
bdosremedios <dosremedios.brandon@gmail.com>
2023-09-12 15:09:00 -0700

    Added MP_GMRES_IR with simple implementation of phase change in constant threshold class ConstantThreshold

---
+++ include/solvers/nested/MP_GMRES_IR.h
@@ -0,0 +1,105 @@
+#ifndef MP_GMRES_IR_SOLVE_H
+#define MP_GMRES_IR_SOLVE_H
+
+#include "IterativeRefinement.h"
+#include "../krylov/GMRES.h"
+
+// template <typename T, typename U=T>
+class MP_GMRES_IR_Solve: public IterativeRefinement {
+
+    private:
+
+        // *** PRIVATE HELPER METHODS ***
+
+        template <typename T>
+        void set_inner_solve() {
+            inner_solver = make_shared<GMRESSolve<T>>(
+                A, curr_res,
+                basis_zero_tol,
+                inner_solve_arg_pkg
+                // inner_precond_arg_pkg
+            );
+        }
+
+    protected:
+
+        // *** PROTECTED CONSTANTS ***
+
+        const int HLF_PHASE = 0;
+        const int SGL_PHASE = 1;
+        const int DBL_PHASE = 2;
+        const int INIT_PHASE = HLF_PHASE;
+
+        // *** PROTECTED ATTRIBUTES ***
+
+        double basis_zero_tol;
+        int cascade_phase;
+
+        // *** PROTECTED ABSTRACT METHODS ***
+
+        virtual void determine_phase() = 0;
+
+        // *** PROTECTED OVERRIDE METHODS ***
+
+        // Initialize inner outer solver;
+        void initialize_inner_outer_solver() override { set_inner_solve<half>(); }
+
+        // Specify inner_solver for outer_iterate_calc and setup
+        void outer_iterate_setup() override {
+            determine_phase();
+            if (cascade_phase == HLF_PHASE) { set_inner_solve<half>(); }
+            else if (cascade_phase == SGL_PHASE) { set_inner_solve<float>(); }
+            else { set_inner_solve<double>(); }
+        }
+
+        void derived_generic_reset() override {
+            cascade_phase = INIT_PHASE;
+            initialize_inner_outer_solver();
+        }
+    
+    public:
+
+        // *** CONSTRUCTORS ***
+
+        MP_GMRES_IR_Solve(
+            Matrix<double, Dynamic, Dynamic> const &arg_A,
+            Matrix<double, Dynamic, 1> const &arg_b,
+            double const &arg_basis_zero_tol,
+            SolveArgPkg const &arg_solve_arg_pkg
+        ):
+            basis_zero_tol(arg_basis_zero_tol),
+            IterativeRefinement(arg_A, arg_b, arg_solve_arg_pkg)
+        {
+            cascade_phase = INIT_PHASE;
+            initialize_inner_outer_solver();
+        }
+
+};
+
+class SimpleConstantThreshold : public MP_GMRES_IR_Solve {
+
+    protected:
+
+        // *** PROTECTED CONSTANTS ***
+    
+        const double tol_hlf = pow(10, -02);
+        const double tol_sgl = pow(10, -05);
+        const double tol_dbl = pow(10, -10);
+
+        // *** PROTECTED OVERRIDE METHODS ***
+
+        void determine_phase() override {
+            if (cascade_phase == HLF_PHASE) {
+                if ((this->get_relres() <= tol_hlf)) { cascade_phase = SGL_PHASE; }
+            } else if (cascade_phase == SGL_PHASE) {
+                if ((this->get_relres() <= tol_sgl)) { cascade_phase = DBL_PHASE; }
+            } else {
+                ;
+            }
+        }
+
+        using MP_GMRES_IR_Solve::MP_GMRES_IR_Solve;
+
+};
+
+#endif
---
+++ test/test_solvers/test_nested/test_MP_GMRES_IR_Solve.cpp
@@ -0,0 +1,43 @@
+#include "../../test.h"
+
+#include "solvers/nested/MP_GMRES_IR.h"
+
+class MP_GMRES_IR_Test: public TestBase {
+
+    public:
+
+        SolveArgPkg dbl_GMRES_IR_args = SolveArgPkg(40, 10, conv_tol_dbl);
+
+};
+
+TEST_F(MP_GMRES_IR_Test, ConvergenceTest_ConvDiff64) {
+
+    Matrix<double, Dynamic, Dynamic> A(read_matrix_csv<double>(solve_matrix_dir + "conv_diff_64_A.csv"));
+    Matrix<double, Dynamic, 1> b(read_matrix_csv<double>(solve_matrix_dir + "conv_diff_64_b.csv"));
+
+    SimpleConstantThreshold mp_gmres_ir_solve(A, b, u_dbl, dbl_GMRES_IR_args);
+
+    mp_gmres_ir_solve.solve();
+
+    if (*show_plots) { mp_gmres_ir_solve.view_relres_plot("log"); }
+
+    EXPECT_TRUE(mp_gmres_ir_solve.check_converged());
+    EXPECT_LE(mp_gmres_ir_solve.get_relres(), conv_tol_dbl);
+
+}
+
+TEST_F(MP_GMRES_IR_Test, ConvergenceTest_ConvDiff256) {
+
+    Matrix<double, Dynamic, Dynamic> A(read_matrix_csv<double>(solve_matrix_dir + "conv_diff_256_A.csv"));
+    Matrix<double, Dynamic, 1> b(read_matrix_csv<double>(solve_matrix_dir + "conv_diff_256_b.csv"));
+
+    SimpleConstantThreshold mp_gmres_ir_solve(A, b, u_dbl, dbl_GMRES_IR_args);
+
+    mp_gmres_ir_solve.solve();
+
+    if (*show_plots) { mp_gmres_ir_solve.view_relres_plot("log"); }
+
+    EXPECT_TRUE(mp_gmres_ir_solve.check_converged());
+    EXPECT_LE(mp_gmres_ir_solve.get_relres(), conv_tol_dbl);
+
+}
