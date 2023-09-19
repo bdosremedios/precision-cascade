a61717196b7455f2a9aa1aa61b2e37987dd5c84c
bdosremedios <dosremedios.brandon@gmail.com>
2023-09-19 14:08:11 -0700

    test: modify Substitution testing to use sparse and dense

--- test/test_tools/test_Substitution.cpp
+++ test/test_tools/test_Substitution.cpp
@@ -2,43 +2,42 @@
 
 #include "tools/Substitution.h"
 
-class SubstitutionTest: public TestBase {
-
+class SubstitutionTest: public TestBase
+{
 public:
 
     template <template <typename> typename M>
     void TestBackwardSubstitution() {
 
         constexpr int n(90);
-        M<double, Dynamic, Dynamic> U_tri(read_matrix_csv<M, double>(solve_matrix_dir + "U_tri_90.csv"));
-        MatrixVector<double, Dynamic, 1> x_tri(read_matrix_csv<MatrixVector, double>(solve_matrix_dir + "x_tri_90.csv"));
-        Matrix<double, Dynamic, 1> Ub_tri(read_matrix_csv<double>(solve_matrix_dir + "Ub_tri_90.csv"));
-        Matrix<double, n, 1> test_soln(back_substitution(U_tri, Ub_tri));
+        M<double> U_tri = read_matrixCSV<M, double>(solve_matrix_dir + "U_tri_90.csv");
+        MatrixVector<double> x_tri = read_matrixCSV<MatrixVector, double>(solve_matrix_dir + "x_tri_90.csv");
+        MatrixVector<double> Ub_tri = read_matrixCSV<MatrixVector, double>(solve_matrix_dir + "Ub_tri_90.csv");
+        MatrixVector<double> test_soln = back_substitution(U_tri, Ub_tri);
 
-        for (int i=0; i<n; ++i) {
-            ASSERT_NEAR(test_soln[i], x_tri[i], u_dbl);
-        }
+        for (int i=0; i<n; ++i) { ASSERT_NEAR(test_soln[i], x_tri[i], u_dbl); }
         ASSERT_NEAR((Ub_tri-U_tri*test_soln).norm(), 0, u_dbl);
+
+    }
+
+    template <template <typename> typename M>
+    void TestForwardSubstitution() {
+
+        constexpr int n(90);
+        M<double> L_tri = read_matrixCSV<M, double>(solve_matrix_dir + "L_tri_90.csv");
+        MatrixVector<double> x_tri = read_matrixCSV<MatrixVector, double>(solve_matrix_dir + "x_tri_90.csv");
+        MatrixVector<double> Lb_tri = read_matrixCSV<MatrixVector, double>(solve_matrix_dir + "Lb_tri_90.csv");
+        MatrixVector<double> test_soln = frwd_substitution(L_tri, Lb_tri);
+
+        for (int i=0; i<n; ++i) { ASSERT_NEAR(test_soln[i], x_tri[i], u_dbl); }
+        ASSERT_NEAR((Lb_tri-L_tri*test_soln).norm(), 0, u_dbl);
 
     }
 
 };
 
-TEST_F(SubstitutionTest, TestBackwardSubstitution) {
+TEST_F(SubstitutionTest, TestBackwardSubstitution_Dense) { TestBackwardSubstitution<MatrixDense>(); }
+TEST_F(SubstitutionTest, TestBackwardSubstitution_Sparse) { TestBackwardSubstitution<MatrixSparse>(); }
 
-}
-
-TEST_F(SubstitutionTest, TestForwardSubstitution) {
-
-    constexpr int n(90);
-    Matrix<double, Dynamic, Dynamic> L_tri(read_matrix_csv<double>(solve_matrix_dir + "L_tri_90.csv"));
-    Matrix<double, Dynamic, 1> x_tri(read_matrix_csv<double>(solve_matrix_dir + "x_tri_90.csv"));
-    Matrix<double, Dynamic, 1> Lb_tri(read_matrix_csv<double>(solve_matrix_dir + "Lb_tri_90.csv"));
-    Matrix<double, n, 1> test_soln(frwd_substitution(L_tri, Lb_tri));
-
-    for (int i=0; i<n; ++i) {
-        ASSERT_NEAR(test_soln[i], x_tri[i], u_dbl);
-    }
-    ASSERT_NEAR((Lb_tri-L_tri*test_soln).norm(), 0, u_dbl);
-
-}
+TEST_F(SubstitutionTest, TestForwardSubstitution_Dense) { TestForwardSubstitution<MatrixDense>(); }
+TEST_F(SubstitutionTest, TestForwardSubstitution_Sparse) { TestForwardSubstitution<MatrixSparse>(); }
