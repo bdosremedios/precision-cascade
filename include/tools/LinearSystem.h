2a2c43b70ebb10ab9ba6dd116720f6bfd86ef754
bdosremedios <dosremedios.brandon@gmail.com>
2023-10-03 09:22:05 -0700

    feat: add return by reference to LinearSystem parameter package class, plan to migrate towards single access to that parameter package

--- include/tools/LinearSystem.h
+++ include/tools/LinearSystem.h
@@ -16,21 +16,29 @@ class LinearSystem {
         b(arg_b)
     {}
 
+    M<double> &A() const { return A; }
+
+    MatrixVector<double> &b() const { return b; }
+
 };
 
 template <template <typename> typename M, typename T>
 class TypedLinearSystem: public LinearSystem {
-    
-    const M<T> A_T;
-    const MatrixVector<T> b_T;
+
+    const M<T> A_Typed;
+    const MatrixVector<T> b_Typed;
 
     LinearSystem(
         M<double> arg_A, MatrixVector<double> arg_b
     ):
-        A_T(arg_A.template cast<T>()),
-        b_T(arg_b.template cast<T>()),
+        A_Typed(arg_A.template cast<T>()),
+        b_Typed(arg_b.template cast<T>()),
         LinearSystem(arg_A, arg_b)
     {}
+
+    M<T> &A_Typed() const { return A_Typed; }
+
+    MatrixVector<T> &b_Typed() const { return b_Typed; }
 
 }
 
