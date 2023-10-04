8eed0b5c0ec900e8dc7e38b5fa3eb4c885cfb58f
bdosremedios <dosremedios.brandon@gmail.com>
2023-10-04 10:52:26 -0700

    refactor: modify FP_GMRES_IR_Solve and testing for clearer naming and use of LinearSystem classes

--- include/solvers/nested/GMRES_IR/FP_GMRES_IR.h
+++ include/solvers/nested/GMRES_IR/FP_GMRES_IR.h
@@ -12,30 +12,33 @@ private:
     // *** PRIVATE HELPER METHODS ***
 
     void set_inner_solve() {
-        curr_typed_lin_sys.set_b(this->curr_res);
+
+        IR_inner_typed_lin_sys.set_b(this->curr_res);
+
         this->inner_solver = make_shared<GMRESSolve<M, T>>(
-            curr_typed_lin_sys,
+            IR_inner_typed_lin_sys,
             basis_zero_tol,
             this->inner_solve_arg_pkg,
             inner_precond_arg_pkg
         );
+
     }
 
 protected:
 
-    // *** PROTECTED ATTRIBUTES ***
+    // *** PROTECTED CONST ATTRIBUTES ***
 
-    const TypedLinearSystem<M, T> initial_typed_lin_sys;
-    Mutb_TypedLinearSystem<M, T> curr_typed_lin_sys;
-    double basis_zero_tol;
-    PrecondArgPkg<M, W> inner_precond_arg_pkg;
+    const double basis_zero_tol;
+    const PrecondArgPkg<M, W> inner_precond_arg_pkg;
+
+    // *** PROTECTED MUTABLE ATTRIBUTES ***
+
+    Mutb_TypedLinearSystem<M, T> IR_inner_typed_lin_sys;
 
     // *** PROTECTED OVERRIDE METHODS ***
 
-    // Initialize inner outer solver;
     void initialize_inner_outer_solver() override { set_inner_solve(); }
 
-    // Specify inner_solver for outer_iterate_calc and setup
     void outer_iterate_setup() override { set_inner_solve(); }
 
     void derived_generic_reset() override { set_inner_solve(); }
@@ -45,17 +48,15 @@ public:
     // *** CONSTRUCTORS ***
 
     FP_GMRES_IR_Solve(
-        const TypedLinearSystem<M, T> &arg_initial_typed_lin_sys,
-        double const &arg_basis_zero_tol,
-        SolveArgPkg const &arg_solve_arg_pkg,
-        PrecondArgPkg<M, W> const &arg_inner_precond_arg_pkg = PrecondArgPkg<M, W>()
+        const TypedLinearSystem<M, T> &arg_outer_typed_lin_sys,
+        const double &arg_basis_zero_tol,
+        const SolveArgPkg &arg_solve_arg_pkg,
+        const PrecondArgPkg<M, W> &arg_inner_precond_arg_pkg = PrecondArgPkg<M, W>()
     ):
-        initial_typed_lin_sys(arg_initial_typed_lin_sys),
         basis_zero_tol(arg_basis_zero_tol),
         inner_precond_arg_pkg(arg_inner_precond_arg_pkg),
-        IterativeRefinement<M>(arg_initial_typed_lin_sys, arg_solve_arg_pkg),
-        curr_typed_lin_sys(Mutb_TypedLinearSystem<M, T>(initial_typed_lin_sys.get_A(),
-                                                        this->curr_res))
+        IterativeRefinement<M>(arg_outer_typed_lin_sys, arg_solve_arg_pkg),
+        IR_inner_typed_lin_sys(arg_outer_typed_lin_sys.get_A(), this->curr_res)
 
     {
         initialize_inner_outer_solver();
