49f34039c16eff52f367d80e7ee65873a66d6783
bdosremedios <dosremedios.brandon@gmail.com>
2023-10-30 15:31:40 -0700

    refactor: create explicit static constructors for MatrixVector and rewrite MatrixReader to more logically check and enter data

--- include/tools/MatrixReader.h
+++ include/tools/MatrixReader.h
@@ -16,40 +16,6 @@ using std::runtime_error;
 using std::cout, std::endl;
 
 template <template<typename> typename M, typename T>
-class SpecializedReturner
-{
-    public: M<T> return_(M<T>);
-};
-
-template <typename T>
-class SpecializedReturner<MatrixVector, T>
-{
-public:
-    MatrixVector<T> return_(MatrixDense<T> mat) {
-        if (mat.cols() == 1) { return mat; }
-        else { throw runtime_error("Vector can only read one column csv."); }
-    }
-};
-
-template <typename T>
-class SpecializedReturner<MatrixDense, T>
-{
-public:
-    MatrixDense<T> return_(MatrixDense<T> mat) { return mat; }
-};
-
-template <typename T>
-class SpecializedReturner<MatrixSparse, T>
-{
-public:
-    MatrixSparse<T> return_(MatrixDense<T> mat) {
-        MatrixSparse<T> temp(mat.sparseView());
-        temp.reduce();
-        return temp;
-    }
-};
-
-template <template<typename> typename M, typename T>
 M<T> read_matrixCSV(fs::path const &path) {
 
     assert_valid_type_or_vec<M>();
@@ -58,93 +24,72 @@ M<T> read_matrixCSV(fs::path const &path) {
     ifstream file_in;
     file_in.open(path);
 
-    // Check that file opening was successful
-    if (file_in.is_open()) {
+    // Ensure success
+    if (!file_in.is_open()) { throw runtime_error("Failed to read: " + path.string()); }
+    
+    int n_rows = 0;
+    int n_cols = 0;
+    string line_string;
+    stringstream line_stream;
+    bool is_first_line = true;
+    string temp_str;
 
-        MatrixDense<T> mat;
-        int n_rows = 0;
-        int n_cols = 0;
+    // Scan file getting n_rows and n_cols and ensuring rectangular nature
+    while (getline(file_in, line_string)) {
         
-        string line_read_temp;
-        stringstream line;
-        bool is_first_line = true;
-        int element_count = 1;
+        ++n_rows;
+        line_stream.clear();
+        line_stream << line_string;
 
-        while (getline(file_in, line_read_temp)) {
-
-            // Read string into stringstream
-            line.clear();
-            line << line_read_temp;
-
-            // Count the first line to ensure all subsequent rows match the element count
-            if (is_first_line) {
-                
-                ++n_rows;
-                string temp_str;
-                T temp_number;
-
-                // Iterate over comma seperated line
-                while (getline(line, temp_str, ',')) {
-                    // Expand mat's column by one and set as new entry if entry is valid
-                    try {
-                        temp_number = static_cast<T>(stod(temp_str));
-                    } catch (std::invalid_argument e) {
-                        throw runtime_error(
-                            "Error in: " + path.string() + "\n" +
-                            "Invalid argument in file, failed to convert to numeric"
-                        );
-                    }
-                    mat.conservativeResize(n_rows, ++n_cols);
-                    mat.coeffRef(0, n_cols-1) = temp_number;
-                }
-                is_first_line = false;
-
-            // For subsequent rows expand by row and read in next line, throwing
-            // errors if any row exceeds to does not meet the original element count
-            } else {
-
-                mat.conservativeResize(++n_rows, n_cols);
-                string temp_str;
-                T temp_number;
-                int curr_col_count = 0;
-
-                // Iterate over comma seperated line
-                while (getline(line, temp_str, ',')) {
-                    // Check that the next added element won't exceed the column count
-                    if (curr_col_count < n_cols) {
-                        // Expand mat's column by one and set as new entry if entry is valid
-                        try {
-                            temp_number = static_cast<T>(stod(temp_str));
-                        } catch (std::invalid_argument e) {
-                            throw runtime_error(
-                                "Error in: " + path.string() + "\n" +
-                                "Invalid argument in file, failed to convert to numeric"
-                            );
-                        }
-                        mat.coeffRef(n_rows-1, curr_col_count++) = temp_number;
-                    } else {
-                        throw runtime_error(
-                            "Error in: " + path.string() + "\n" +
-                            "Row " + to_string(n_rows) + " exceeds column size of " + to_string(n_cols)
-                        );
-                    }
-                }
-
-                // Check that newly added row meets the column count
-                if (curr_col_count < n_cols) {
-                    throw runtime_error(
-                        "Error in: " + path.string() + "\n" +
-                        "Row " + to_string(n_rows) + " does not meet column size of " + to_string(n_cols)
-                    );
-                }
-
+        if (is_first_line) {
+            // Iterate over first row to get number of columns
+            while (getline(line_stream, temp_str, ',')) { ++n_cols; }
+            is_first_line = false;
+        } else {
+            // Ensure subsequent rows meet col count of first column
+            int col_count = 0;
+            while (getline(line_stream, temp_str, ',')) { ++col_count; }
+            if (col_count != n_cols) {
+                throw runtime_error(
+                    "Error in: " + path.string() + "\n" +
+                    "Row " + to_string(n_rows) + " does not meet column size of " + to_string(n_cols)
+                );
             }
         }
+    
+    }
 
-        SpecializedReturner<M, T> sr;
-        return sr.return_(mat);
+    // Read entries into matrix
 
-    } else { throw runtime_error("Failed to read: " + path.string()); }
+    file_in.clear();
+    file_in.seekg(0, std::ios_base::beg);
+    M<T> mat = M<T>::Zero(n_rows, n_cols);
+    T temp_number;
+    int row = 0;
+
+    while (getline(file_in, line_string)) {
+
+        line_stream.clear();
+        line_stream << line_string;
+
+        int col = 0;
+        while (getline(line_stream, temp_str, ',')) {
+            try { temp_number = static_cast<T>(stod(temp_str)); }
+            catch (std::invalid_argument e) {
+                throw runtime_error(
+                    "Error in: " + path.string() + "\n" +
+                    "Invalid argument in file, failed to convert to numeric"
+                );
+            }
+            mat.coeffRef(row, col) = temp_number;
+            col++;
+        }
+
+        row++;
+
+    }
+
+    return mat;
 
 }
 
--- include/types/MatrixVector.h
+++ include/types/MatrixVector.h
@@ -7,6 +7,27 @@ using Eigen::Matrix;
 using Eigen::Dynamic;
 
 template <typename T>
-using MatrixVector = Matrix<T, Dynamic, 1>;
+class MatrixVector: public Matrix<T, Dynamic, 1>
+{
+private:
+
+    using Parent = Matrix<T, Dynamic, 1>;
+
+    static void check_n(int n) { if (n != 1) { throw std::runtime_error("Invalid number of columns for vector."); } }
+
+public:
+
+    using Parent::Matrix;
+    MatrixVector(int m, int n): Parent::Matrix(m, 1) { check_n(n); }
+    MatrixVector(int m): Parent::Matrix(m, 1) {}
+
+    static MatrixVector<T> Zero(int m, int n) { check_n(n); return Parent::Zero(m, 1); }
+    static MatrixVector<T> Zero(int m) { return Parent::Zero(m, 1); }
+    static MatrixVector<T> Ones(int m, int n) { check_n(n); return Parent::Ones(m, 1); }
+    static MatrixVector<T> Ones(int m) { return Parent::Ones(m, 1); }
+    static MatrixVector<T> Random(int m, int n) { check_n(n); return Parent::Random(m, 1); }
+    static MatrixVector<T> Random(int m) { return Parent::Random(m, 1); }
+
+};
 
 #endif
