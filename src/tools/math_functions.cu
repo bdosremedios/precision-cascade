8751636915685ddebe6344a1b0a6958e14ba60a5
Brandon Dos Remedios <dosremedios.brandon@gmail.com>
2024-02-02 10:28:19 -0800

    perf: remove vestigial math_functions class and fix cmake files

--- src/tools/math_functions.cu
+++
@@ -1,7 +0,0 @@
-#include "tools/math_functions.h"
-
-double square_root(double x) { return std::sqrt(x); }
-
-float square_root(float x) { return std::sqrt(x); }
-
-__half square_root(__half x) { return static_cast<__half>(std::sqrt(static_cast<float>(x))); }
