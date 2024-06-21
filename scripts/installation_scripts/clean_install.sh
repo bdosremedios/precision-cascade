dfcf35284ad63b1f0beee9c27e461fd45bb500a4
bdosremedios <dosremedios.brandon@gmail.com>
2024-06-21 13:13:20 -0700

    feat: add script to clean installation

---
+++ scripts/installation_scripts/clean_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+cd ../..
+
+rm -r build
+rm -r install
--- scripts/run_numerical_expimentation.ps1
+++
@@ -1,1 +0,0 @@
-.\experimentation\main\Debug\experiment.exe C:\\users\\dosre\\dev\\numerical_experimentation\\data C:\\users\\dosre\\dev\\numerical_experimentation\\input C:\\users\\dosre\\dev\\numerical_experimentation\\output
