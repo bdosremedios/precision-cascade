6dcbac4b7ea1c80447fc719de27d03b002f8e202
bdosremedios <dosremedios.brandon@gmail.com>
2024-07-05 15:04:30 -0700

    feat: add long test run command and restructure scripts

--- scripts/add-exec-priviliges.sh
+++ scripts/add-exec-priviliges.sh
@@ -15,7 +15,8 @@ chmod u+x ./apptainer/1-build-run.sh
 chmod u+x ./apptainer/2-0-gen-clear-local-host-data-dir.sh
 chmod u+x ./apptainer/2-1-gen-clear-remote-ln-host-data-dir.sh
 chmod u+x ./apptainer/3-0-run-test.sh
-chmod u+x ./apptainer/3-1-run-test-experiment.sh
-chmod u+x ./apptainer/3-2-run-benchmark.sh
-chmod u+x ./apptainer/3-3-run-experiment.sh
+chmod u+x ./apptainer/3-1-run-test-rlt.sh
+chmod u+x ./apptainer/3-2-run-test-experiment.sh
+chmod u+x ./apptainer/4-0-run-experiment.sh
+chmod u+x ./apptainer/4-1-run-benchmark.sh
 chmod u+x ./apptainer/clean-sif.sh
