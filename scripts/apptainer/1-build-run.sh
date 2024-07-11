08e22a9aeb3250306f28a4f9263071528513861d
bdosremedios <dosremedios.brandon@gmail.com>
2024-07-11 10:44:57 -0700

    fix: fix helper script finding in apptainer run build

--- scripts/apptainer/1-build-run.sh
+++ scripts/apptainer/1-build-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 ./helper/check-nvidia-arg.sh $1 || exit 1
 
-cd "$(dirname "$0")"
 cd ../../container
 
 if [[ -e precision-cascade-run.sif ]]; then rm precision-cascade-run.sif; fi
