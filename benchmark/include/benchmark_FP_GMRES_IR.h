62b8505ede444a814a2748640a318bd3a1599385
bdosremedios <dosremedios.brandon@gmail.com>
2024-07-18 08:46:42 -0700

    test: add all precision fp gmres benchmarking

---
+++ benchmark/include/benchmark_FP_GMRES_IR.h
@@ -0,0 +1,40 @@
+#ifndef BENCHMARK_FP_GMRES_IR_H
+#define BENCHMARK_FP_GMRES_IR_H
+
+#include "benchmark_Nested_GMRES.h"
+
+#include "tools/arg_pkgs/LinearSystem.h"
+#include "tools/arg_pkgs/SolveArgPkg.h"
+#include "solvers/nested/GMRES_IR/FP_GMRES_IR.h"
+
+class Benchmark_FP_GMRES_IR: public Benchmark_Nested_GMRES
+{
+public:
+
+    template <typename TPrecision>
+    void execute_fp_gmres_ir(
+        Benchmark_AccumClock &clock, NoFillMatrixSparse<double> &A
+    ) {
+
+        Vector<double> x_soln = Vector<double>::Random(
+            BenchmarkBase::bundle, A.rows()
+        );
+
+        GenericLinearSystem<NoFillMatrixSparse> gen_lin_sys(A, A*x_soln);
+        TypedLinearSystem<NoFillMatrixSparse, TPrecision> typed_lin_sys(
+            &gen_lin_sys
+        );
+        SolveArgPkg args(nested_outer_iter, nested_inner_iter, 0.);
+
+        clock.clock_start();
+        FP_GMRES_IR_Solve<NoFillMatrixSparse, TPrecision> fp_restarted_gmres(
+            &typed_lin_sys, 0., args
+        );
+        fp_restarted_gmres.solve();
+        clock.clock_stop();
+
+    };
+
+};
+
+#endif
--- benchmark/src/benchmark_solvers/benchmark_FP_GMRES_IR.cpp
+++ benchmark/src/benchmark_solvers/benchmark_FP_GMRES_IR.cpp
@@ -1,37 +1,46 @@
-#include "benchmark_Nested_GMRES.h"
+#include "benchmark_FP_GMRES_IR.h"
 
-#include "tools/arg_pkgs/LinearSystem.h"
-#include "tools/arg_pkgs/SolveArgPkg.h"
-#include "solvers/nested/GMRES_IR/FP_GMRES_IR.h"
-
-class Benchmark_FP_GMRES_IR: public Benchmark_Nested_GMRES {};
-
-TEST_F(Benchmark_FP_GMRES_IR, FP_GMRES_IR_BENCHMARK) {
+TEST_F(Benchmark_FP_GMRES_IR, FP_GMRES_IR_Double_BENCHMARK) {
 
     std::function<void (Benchmark_AccumClock &, NoFillMatrixSparse<double> &)> execute_func = [this] (
         Benchmark_AccumClock &clock, NoFillMatrixSparse<double> &A
     ) {
-
-        Vector<double> x_soln = Vector<double>::Random(
-            BenchmarkBase::bundle, A.rows()
-        );
-
-        GenericLinearSystem<NoFillMatrixSparse> gen_lin_sys(A, A*x_soln);
-        TypedLinearSystem<NoFillMatrixSparse, double> typed_lin_sys(
-            &gen_lin_sys
-        );
-        SolveArgPkg args(nested_outer_iter, nested_inner_iter, 0.);
-
-        clock.clock_start();
-        FP_GMRES_IR_Solve fp_restarted_gmres(&typed_lin_sys, 0., args);
-        fp_restarted_gmres.solve();
-        clock.clock_stop();
-
+        execute_fp_gmres_ir<double>(clock, A);
     };
 
     benchmark_exec_func<NoFillMatrixSparse>(
         sparse_start, sparse_stop, sparse_incr,
-        make_norm_A, execute_func, "fp_gmres_ir"
+        make_norm_A, execute_func, "fp_gmres_ir_dbl"
+    );
+
+}
+
+TEST_F(Benchmark_FP_GMRES_IR, FP_GMRES_IR_Single_BENCHMARK) {
+
+    std::function<void (Benchmark_AccumClock &, NoFillMatrixSparse<double> &)> execute_func = [this] (
+        Benchmark_AccumClock &clock, NoFillMatrixSparse<double> &A
+    ) {
+        execute_fp_gmres_ir<float>(clock, A);
+    };
+
+    benchmark_exec_func<NoFillMatrixSparse>(
+        sparse_start, sparse_stop, sparse_incr,
+        make_norm_A, execute_func, "fp_gmres_ir_sgl"
+    );
+
+}
+
+TEST_F(Benchmark_FP_GMRES_IR, FP_GMRES_IR_Half_BENCHMARK) {
+
+    std::function<void (Benchmark_AccumClock &, NoFillMatrixSparse<double> &)> execute_func = [this] (
+        Benchmark_AccumClock &clock, NoFillMatrixSparse<double> &A
+    ) {
+        execute_fp_gmres_ir<__half>(clock, A);
+    };
+
+    benchmark_exec_func<NoFillMatrixSparse>(
+        sparse_start, sparse_stop, sparse_incr,
+        make_norm_A, execute_func, "fp_gmres_ir_hlf"
     );
 
 }
