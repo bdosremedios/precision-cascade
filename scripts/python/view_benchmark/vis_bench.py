ec45a853cfb32452f3269d1e135c941d006319a7
Brandon Dos Remedios <dosremedios.brandon@gmail.com>
2024-08-20 16:08:27 -0700

    feat: finalize benchmark python data visualization

---
+++ scripts/python/view_benchmark/vis_bench.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from bokeh.palettes import Category20
+
+cat20extract = Category20[20]
+
+def load_benchmark_csv_us(data_dir, fname):
+
+    data = np.loadtxt(
+        os.path.join(data_dir, fname+".csv"),
+        delimiter=",",
+        ndmin=2,
+        skiprows=1
+    )
+
+    return data[:, 0], data[:, 1]*1e-6
+
+def load_dense_data(data_dir, fname):
+    data_x, data_y = load_benchmark_csv_us(data_dir, fname)
+    return data_x**2, data_y
+
+def load_sparse_data(data_dir, fname):
+    data_x, data_y = load_benchmark_csv_us(data_dir, fname)
+    return data_x**1.5, data_y
+
+def load_tri_dense_data(data_dir, fname):
+    data_x, data_y = load_benchmark_csv_us(data_dir, fname)
+    return 0.5*data_x**2, data_y
+
+def load_tri_sparse_data(data_dir, fname):
+    data_x, data_y = load_benchmark_csv_us(data_dir, fname)
+    return 0.5*(data_x**1.5), data_y
+
+def plot_data(ax, data_dir, li_tup_fname_label, load_data_func, disable_y=False):
+    
+    for fname, label, clridx in li_tup_fname_label:
+
+        data_x, data_y = load_data_func(data_dir, fname)
+
+        ax.plot(
+            data_x, data_y,
+            label=label,
+            color=cat20extract[clridx],
+            marker=".",
+            linestyle="-"
+        )
+
+    ax.set_xlabel("Relevant Non-Zeros")
+    if not disable_y:
+        ax.set_ylabel("Seconds")
+    ax.grid(True)
+    ax.legend()
