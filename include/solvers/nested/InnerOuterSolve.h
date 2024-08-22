b72ff78e752ee5c8cdd863074e8160f848d9c49d
bdosremedios <dosremedios.brandon@gmail.com>
2024-08-22 09:46:15 -0700

    temp: add check point marks to FP_GMRES parent to see what taking longer in half

--- include/solvers/nested/InnerOuterSolve.h
+++ include/solvers/nested/InnerOuterSolve.h
@@ -3,12 +3,33 @@
 
 #include "../IterativeSolve.h"
 
+#include <chrono>
+#include <iostream>
+
 namespace cascade {
 
 template <template <typename> typename TMatrix>
 class InnerOuterSolve: public GenericIterativeSolve<TMatrix>
 {
 protected:
+
+    std::chrono::high_resolution_clock clock;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+
+    int mk_1 = 0;
+    int mk_2 = 0;
+    int mk_3 = 0;
+    int mk_4 = 0;
+
+    void mark_start() {
+        start = clock.now();
+    }
+
+    int mark_stop() {
+        return std::chrono::duration_cast<std::chrono::microseconds>(
+            clock.now() - start
+        ).count();
+    }
 
     int max_inner_iter; // mutable to allow setting by derived solvers
     std::vector<std::vector<double>> inner_res_norm_history;
@@ -17,11 +38,37 @@ protected:
     std::shared_ptr<GenericIterativeSolve<TMatrix>> inner_solver;
 
     void iterate() override {
+        mark_start();
         outer_iterate_setup();
+        mk_1 += mark_stop(); 
+        mark_start();
         inner_solver->solve();
+        mk_2 += mark_stop(); 
+        mark_start();
         inner_res_norm_history.push_back(inner_solver->get_res_norm_history());
         inner_iterations.push_back(inner_solver->get_iteration());
+        mk_3 += mark_stop();
+        mark_start();
         outer_iterate_complete();
+        mk_4 += mark_stop();
+
+        if (this->get_iteration() == this->max_iter) {
+            int total = mk_1 + mk_2 + mk_3 + mk_4;
+            std::cout << "Iter: " << this->get_iteration() << " | "
+                      << "Mark 1: "
+                      << static_cast<float>(mk_1)/static_cast<float>(total)
+                      << " " << mk_1 << " | "
+                      << "Mark 2: "
+                      << static_cast<float>(mk_2)/static_cast<float>(total)
+                      << " " << mk_2 << " | "
+                      << "Mark 3: "
+                      << static_cast<float>(mk_3)/static_cast<float>(total)
+                      << " " << mk_3 << " | "
+                      << "Mark 4: "
+                      << static_cast<float>(mk_4)/static_cast<float>(total)
+                      << " " << mk_4 << std::endl;
+        }
+
     };
 
     void derived_generic_reset() override {
